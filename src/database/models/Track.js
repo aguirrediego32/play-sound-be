c6aa2b36eb3e97b53e5774c57b117e1914e3519a
Diego Aguirre <aguirrediego734@gmail.com>
2022-08-28 23:10:49 -0300

    creacion de modelos para canciones

---
+++ src/database/models/Track.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Track extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Track.belongsTo(models.Album,{
+        foreignKey: 'id',
+        target_key: 'albumId'
+      });
+      Track.belongsTo(models.Artist,{
+        foreignKey: 'id',
+        target_key: 'artistId'
+      })
+    }
+  }
+  Track.init({
+    name: DataTypes.STRING,
+    albumId: DataTypes.INTEGER,
+    cover: DataTypes.STRING,
+    artistId: DataTypes.INTEGER,
+    duration: DataTypes.INTEGER,
+    url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Track',
+  });
+  return Track;
+};
