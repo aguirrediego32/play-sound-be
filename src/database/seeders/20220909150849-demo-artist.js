8f8b46ecb093a4bf3510d48d8ae8bb57875d83f1
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-09-09 12:22:41 -0300

    testdata artists

---
+++ src/database/seeders/20220909150849-demo-artist.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Artists', [
+      {
+        name: 'David Guetta',
+        nickname: 'David Guetta',
+        nationality: 'FR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Snow',
+        nickname: 'Snow',
+        nationality: 'US',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Round Table Knights',
+        nickname: 'Round Table Knights',
+        nationality: 'US',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Ed Sheeran',
+        nickname: 'Ed Sheeran',
+        nationality: 'UK',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'BEBE (Offucuak Video)',
+        nickname: 'Gioli & Assia',
+        nationality: 'IT',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'AC/DC',
+        nickname: 'AC/DC',
+        nationality: 'US',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '50 Cent',
+        nickname: '50 Cent',
+        nationality: 'US',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Bésame',
+        nickname: 'Valentino',
+        nationality: 'CO',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Artists', null, {});
+  }
+};
