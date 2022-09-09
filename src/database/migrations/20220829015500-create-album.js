e4b18fdb90f6f33451a5698ee65f2e8552871db3
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-09-09 10:19:02 -0300

    datos album

--- src/database/migrations/20220829015500-create-album.js
+++ src/database/migrations/20220829015500-create-album.js
@@ -13,11 +13,13 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        default: Sequelize.NOW
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        default: Sequelize.NOW
       }
     });
   },
