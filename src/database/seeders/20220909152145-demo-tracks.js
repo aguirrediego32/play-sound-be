15ef6818b643ce89011bf95d6c6ddcd9ad2d26d8
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-09-10 22:09:14 -0300

    data tracks

--- src/database/seeders/20220909152145-demo-tracks.js
+++ src/database/seeders/20220909152145-demo-tracks.js
@@ -10,7 +10,7 @@ module.exports = {
         cover: 'https://jenesaispop.com/wp-content/uploads/2009/09/guetta_onelove.jpg',
         artistId: 9,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-1.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -20,7 +20,7 @@ module.exports = {
         cover: 'https://is5-ssl.mzstatic.com/image/thumb/Features125/v4/9c/b9/d0/9cb9d017-fcf6-28c6-81d0-e9ac5b0f359e/pr_source.png/800x800cc.jpg',
         artistId: 10,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-2.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -30,7 +30,7 @@ module.exports = {
         cover: 'https://cdns-images.dzcdn.net/images/cover/1db3f8f185e68f26feaf0b9d72ff1645/350x350.jpg',
         artistId: 11,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-3.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -40,7 +40,7 @@ module.exports = {
         cover: 'https://www.lahiguera.net/musicalia/artistas/ed_sheeran/disco/11372/tema/25301/ed_sheeran_bad_habits-portada.jpg',
         artistId: 12,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-4.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -50,7 +50,7 @@ module.exports = {
         cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
         artistId: 13,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-5.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -60,7 +60,7 @@ module.exports = {
         cover: 'https://cdns-images.dzcdn.net/images/cover/ba5eaf2f3a49768164d0728b7ba64372/500x500.jpg',
         artistId: 14,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-6.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -70,7 +70,7 @@ module.exports = {
         cover: 'https://i.scdn.co/image/ab67616d0000b27391f7222996c531b981e7bb3d',
         artistId: 15,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-7.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -80,7 +80,7 @@ module.exports = {
         cover: 'https://i1.sndcdn.com/artworks-000247627460-1hqnjr-t500x500.jpg',
         artistId: 16,
         duration: 333,
-        url: '/',
+        url: 'http://localhost:3000/track-8.mp3',
         createdAt: new Date(),
         updatedAt: new Date()
       }
